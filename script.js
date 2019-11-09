bc1c3d757dcc163036f80b75104e6856c9e67b72
VioLet-Loutsik <violeta.lutic@gmail.com>
2019-11-09 19:24:32 +0200

    add my homework

--- script.js
+++ script.js
@@ -1,54 +1,19 @@
-class Menu {   // homework, point 1 and 2
-  constructor(elem) {
-    this._elem = elem;
-    elem.onclick = this.onClick.bind(this);
-  }
 
-  firstbutton() {
-    alert('You clicked First button');
-    console.log("You clicked First button");
-  }
-
-  secondbutton() {
-    alert('You clicked Second button');
-    console.log("You clicked Second button");
-  }
-
-  thirdbutton() {
-    alert('You clicked Third button');
-    console.log("You clicked Third button");
-  }
-
-  onClick(event) {
-    let action = event.target.dataset.action;
-    if (action) {
-      this[action]();
-    }
-  };
+var str1_ = "You clicked First button",
+changeText = function()  {
+    document.querySelector('h3').textContent = str1_;
 }
+document.getElementById('btn1').addEventListener('click', changeText, false);
 
-new Menu(menu);  
-
-// i tried to do the third point of homework
-
-const firstbutton = document.querySelector('firstbutton');
-const secondbutton = document.querySelector('secondbutton');
-const thirdbutton = document.querySelector('thirdbutton');
-
-let handler = function(event) {
-  console.log(event);
+var str2_ = 'You clicked Second button',
+changeText = function()  {
+    document.querySelector('h3').textContent = str2_;
 }
+document.getElementById('btn2').addEventListener('click', changeText, false);
 
-firstbutton.addEventListener('click', handler, function() {
-  console.log("You clicked First button");
-},true);
-
-secondbutton.addEventListener('click', function() {
-  console.log("You clicked Second button");
-},true);
-
-thirdbutton.addEventListener('click', function() {
-  console.log("You clicked Third button");
-});
-
+var str3_ = 'You clicked Third button',
+changeText = function()  {
+    document.querySelector('h3').textContent = str3_;
+}
+document.getElementById('btn3').addEventListener('click', changeText, false);
 
